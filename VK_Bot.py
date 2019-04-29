9315a5b829ef5f22797f1395e8db3509ac8ec2e0
Krone213 <50117088+Krone213@users.noreply.github.com>
2019-04-29 21:15:02 +0300

    Add files via upload

---
+++ VK_Bot.py
@@ -0,0 +1,33 @@
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+
+def write_msg(user_id, message):
+    vk.method("messages.send", {"user_id":user_id, "message":message, "random_id":user_id})
+    print(user_id)
+
+#entered the token
+token = '*****'
+
+#Авторизуемся как сообщество
+vk = vk_api.VkApi(token=token)
+
+# Работа с сообщениями
+longpoll = VkLongPoll(vk)
+while True:
+	# Основной цикл
+	for event in longpoll.listen():
+		print(1)
+    	# Если пришло новое сообщение
+		if event.type == VkEventType.MESSAGE_NEW:
+        	# Если оно имеет метку для бота
+			if event.to_me:
+            	# Сообщение от пользователя
+				request = event.text
+            	# логика ответа
+				if request == "привет":
+					write_msg(event.user_id, "Хай")
+					print(0)
+				elif request == "пока":
+					write_msg(event.user_id, "Пока((")
+				else:
+					write_msg(event.user_id, "Не поняла вашего ответа...")
